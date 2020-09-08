f552a8affa17488d42ca5dc05fdb5e23f3796f18
UmiKazeYu <uosius21@gmail.com>
2020-09-08 14:23:08 +0900

    Initial commit

---
+++ Assets/BlockGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlockGenerator : MonoBehaviour
+{
+    public GameObject block;
+    public GameObject numberText;
+    public GameObject canvas;
+    int start = 1;
+    int end = 10;
+    float interval = 1.4f;
+    float offsetX = -6.0f;
+    float offsetY = -4.5f;
+    Vector2 blockPosition = new Vector2(-6.0f, -4.5f);
+    Vector2 textPosition = new Vector2(230, 33);
+
+    List<int> numbers = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = start; i <= end; i++)
+        {
+            numbers.Add(i);
+        }
+
+        while (numbers.Count > 0)
+        {
+            int index = Random.Range(0, numbers.Count);
+
+            int random = numbers[index];
+            Debug.Log(random);
+
+            blockPosition.x += interval;
+
+            GameObject thisBlock = Instantiate(block, blockPosition, Quaternion.identity);
+            GameObject thisCanvas = Instantiate(canvas, this.transform.position, Quaternion.identity, thisBlock.transform);
+            GameObject thisText = Instantiate(numberText, textPosition, Quaternion.identity, thisCanvas.transform);
+            thisText.GetComponent<Text>().text = random.ToString();
+
+            thisBlock.gameObject.name = random.ToString();
+
+            textPosition.x += 75.5f;
+
+            numbers.RemoveAt(index);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/UsagiController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsagiController : MonoBehaviour
+{
+    private Rigidbody2D myRigidbody2D;
+    private Animator animator;
+    private float velocityX = 18.0f;
+    private float velocityY = 22.0f;
+    int touchedBlockNumber = -1;
+    bool failure = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.myRigidbody2D = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputVelocityX = 0;
+        //float inputVelocityY = 0;
+
+        //if (Input.GetKeyDown(KeyCode.UpArrow))
+        //{
+        //    animator.SetTrigger("jumpTrigger");
+        //    inputVelocityY = velocityY;
+        //}
+        //else
+        //{
+        //    inputVelocityY = this.myRigidbody2D.velocity.y;
+        //}
+
+        if (Input.GetKey(KeyCode.LeftArrow) && this.transform.position.x > -8.3f)
+        {
+            inputVelocityX = -velocityX;
+            GetComponent<Animator>().SetBool("walkBool", true);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < 8.3f)
+        {
+            inputVelocityX = velocityX;
+            GetComponent<Animator>().SetBool("walkBool", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            GetComponent<Animator>().SetBool("walkBool", false);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            GetComponent<Animator>().SetBool("walkBool", false);
+        }
+
+        this.myRigidbody2D.velocity = new Vector2(inputVelocityX, this.myRigidbody2D.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "BlockTag" && failure)
+        {
+            this.myRigidbody2D.velocity = new Vector2(this.myRigidbody2D.velocity.x, velocityY);
+            animator.SetTrigger("jumpTrigger");
+
+            int blockNumber = int.Parse(collision.gameObject.name);
+
+            if (touchedBlockNumber + 1 == blockNumber)
+            {
+                Debug.Log("あってます");
+            }
+            else
+            {
+                failure = false;
+                Debug.Log("まちがえまちた");
+            }
+
+            touchedBlockNumber = blockNumber;
+        }
+
+        Debug.Log("Hit" + collision.gameObject.name);
+
+
+
+    }
+}
